34b9c9f863fd1b15e41411e00080159572f9d02c
fajar diki <fajarsiddiqi@gmail.com>
2019-10-12 22:47:54 +0700

    wkw

--- application/views/V_dasboard.php
+++ application/views/V_dasboard.php
@@ -5,7 +5,16 @@
 </head>
 <body>
 
-	<h1>Selamat malam minggu yang membosankan</h1>
+<!-- 	<h1>Selamat malam minggu yang membosankan</h1>
+	<h2>Selamat malam minggu yang membosankan</h2>
+	<h3>Selamat malam minggu yang membosankan</h3>
+	<h4>Selamat malam minggu yang membosankan</h4>
+	<h5>Selamat malam minggu yang membosankan</h5>
+	<h6>Selamat malam minggu yang membosankan</h6> -->
+
+	<?php for ($no=1; $no < 500 ; $no++) { ?>
+		<h1>Aku adalah manusia buatan nomer <?php echo $no; ?>, Mohon kerja samanya</h1>
+	<?php } ?>
 
 </body>
 </html>
