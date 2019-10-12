07d7967ab11ebcf6b86e943f040588a5dc9c4de5
fajar diki <fajarsiddiqi@gmail.com>
2019-10-12 22:39:50 +0700

    yak

---
+++ application/controllers/C_dasboard.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * 
+ */
+class C_dasboard extends CI_Controller {
+	
+	// function __construct() {
+	// 	parent::__construct();
+	// }
+
+	public function index()	{
+		$this->load->view('V_dasboard');
+	}
+}
+?>
---
+++ application/views/V_dasboard.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gimana???</title>
+</head>
+<body>
+
+	<h1>Selamat malam minggu yang membosankan</h1>
+
+</body>
+</html>
