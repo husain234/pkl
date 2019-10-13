b07e9e77300b1480df28e9475975cfde133ced01
husain234 <48036941+husain234@users.noreply.github.com>
2019-10-13 10:23:37 +0700

    jadikan slider

--- application/views/V_dasboard.php
+++ application/views/V_dasboard.php
@@ -2,6 +2,30 @@
 <html>
 <head>
 	<title>Gimana???</title>
+	<meta charset="utf-8">
+		<title>Bootstrap E-commerce Templates</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
+		<!-- bootstrap -->
+		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
+		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+		
+		<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
+		
+		<!-- global styles -->
+		<link href="themes/css/flexslider.css" rel="stylesheet"/>
+		<link href="themes/css/main.css" rel="stylesheet"/>
+
+		<!-- scripts -->
+		<script src="themes/js/jquery-1.7.2.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>				
+		<script src="themes/js/superfish.js"></script>	
+		<script src="themes/js/jquery.scrolltotop.js"></script>
+		<!--[if lt IE 9]>			
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
 </head>
 <body>
 
@@ -12,9 +36,27 @@
 	<h5>Selamat malam minggu yang membosankan</h5>
 	<h6>Selamat malam minggu yang membosankan</h6> -->
 
-	<?php for ($no=1; $no < 500 ; $no++) { ?>
+	<!-- <?php for ($no=1; $no < 500 ; $no++) { ?>
 		<h1>Aku adalah manusia buatan nomer <?php echo $no; ?>, Mohon kerja samanya</h1>
-	<?php } ?>
+	<?php } ?> -->
+	<section  class="homepage-slider" id="home-slider">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+						<!-- <img src="<?php echo base_url().'assets/img/'.$img ?>" class="d-block w-100" alt="..."> -->
+							<img src="assets/img/banner-1.jpg" alt="" />
+						</li>
+						<li>
+							<img src="assets/img/carousel/banner-2.jpg" alt="" />
+							<div class="intro">
+								<h1>Mid season sale</h1>
+								<p><span>Up to 50% Off</span></p>
+								<p><span>On selected items online and in stores</span></p>
+							</div>
+						</li>
+					</ul>
+				</div>			
+			</section>
 
 </body>
 </html>
